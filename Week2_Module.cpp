434e5ebb2addb13025c2744ee0ab923a6be01d55
satyamvishwakarma-dev <officialsatyamvishwakarma06@gmail.com>
2026-06-21 14:32:23 +0530

    Q2

--- Week2_Module.cpp
+++ Week2_Module.cpp
@@ -1,32 +1,73 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     int N;
+//     if (!(cin >> N)) return 0;
+
+//     int rollNumbers[1000]; 
+//     for (int i = 0; i < N; i++) {
+//         cin >> rollNumbers[i];
+//     }
+    
+//     int X;
+//     cin >> X;
+    
+//     int foundIndex = -1;
+//     for (int i = 0; i < N; i++) {
+//         if (rollNumbers[i] == X) {
+//             foundIndex = i;
+//             break; // Stop when found
+//         }
+//     }
+    
+//     if (foundIndex != -1) {
+//         cout << "Found at index " << foundIndex << endl;
+//     } else {
+//         cout << "Not Found" << endl;
+//     }
+    
+//     return 0;
+// }
+
+//------------------------------------------------------------------------------
 #include <iostream>
-
 using namespace std;
-
 int main() {
     int N;
     if (!(cin >> N)) return 0;
 
-    int rollNumbers[1000]; 
+    int* products = new int[N];
     for (int i = 0; i < N; i++) {
-        cin >> rollNumbers[i];
+        cin >> products[i];
     }
     
-    int X;
-    cin >> X;
+    int target;
+    cin >> target;
     
-    int foundIndex = -1;
-    for (int i = 0; i < N; i++) {
-        if (rollNumbers[i] == X) {
-            foundIndex = i;
-            break; // Stop when found
+    int low = 0, high = N - 1;
+    bool found = false;
+    
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        
+        if (products[mid] == target) {
+            found = true;
+            break;
+        } else if (products[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
         }
     }
     
-    if (foundIndex != -1) {
-        cout << "Found at index " << foundIndex << endl;
+    if (found) {
+        cout << "Product Available" << endl;
     } else {
-        cout << "Not Found" << endl;
+        cout << "Product Not Available" << endl;
     }
     
+    delete[] products;
     return 0;
 }
